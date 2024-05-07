255ccdf4bc7bb2a054b557ecce0585a962a941b1
azizkhonteacher <azizkhonteacher@gmail.com>
2024-05-07 10:49:05 +0500

    3-DARS

---
+++ 3-dars/JS/app.js
@@ -0,0 +1,38 @@
+const app = Vue.createApp({
+    data() {
+      return {
+        name: "hello world",
+        count: 0,
+        showModal: false,
+        users: [
+          {
+            name: "Iqboljon",
+            age: 18,
+            gender: true,
+          },
+          {
+            name: "Bonu",
+            age: -1,
+            gender: false,
+          },
+          {
+            name: "Sherzodbek",
+            age: 2,
+            gender: true,
+          },
+        ],
+      };
+    },
+    methods: {
+      remCount() {
+        if (this.count > 0) {
+          this.count--;
+        }
+      },
+      addCount() {
+        this.count++;
+      },
+    },
+  });
+  app.mount("#app");
+  
